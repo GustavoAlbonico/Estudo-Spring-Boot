5b326c514755923f20286a7e866ccfb615c73ade
gustavo.albonico <gustavoalbonicogoncalves@hotmail.com>
2024-01-17 17:02:21 -0300

    Alteração de versão do spring para 3.1.2, tantando arrumar o security.

--- pdv/src/main/java/com/gm2/pdv/controller/PDVInfoController.java
+++ pdv/src/main/java/com/gm2/pdv/controller/PDVInfoController.java
@@ -1,15 +1,15 @@
-//package com.gm2.pdv.controller;
-//
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//@RequestMapping("/info")
-//public class PDVInfoController {
-//
-//    public ResponseEntity get(){
-//        return new ResponseEntity<>("PDV info", HttpStatus.OK);
-//    }
-//}
+package com.gm2.pdv.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/info")
+public class PDVInfoController {
+
+    public ResponseEntity get(){
+        return new ResponseEntity<>("PDV info", HttpStatus.OK);
+    }
+}
