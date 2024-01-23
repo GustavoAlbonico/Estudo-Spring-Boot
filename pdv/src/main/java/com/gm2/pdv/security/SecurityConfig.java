ae426a21b7f1373761ead00d1bbfde8393e9587e
gustavo.albonico <gustavoalbonicogoncalves@hotmail.com>
2024-01-23 16:59:08 -0300

    arrumando a config do SecurityConfig

--- pdv/src/main/java/com/gm2/pdv/security/SecurityConfig.java
+++ pdv/src/main/java/com/gm2/pdv/security/SecurityConfig.java
@@ -3,9 +3,10 @@ package com.gm2.pdv.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,7 +16,8 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(securedEnabled = true)
+@EnableGlobalMethodSecurity(securedEnabled = true)
+
 public class SecurityConfig {
 
     @Bean
@@ -24,39 +26,28 @@ public class SecurityConfig {
     }
 
     @Autowired
-    private CustomUserDetailService userDetailServices;
+    private CustomUserDetailService userDetailService;
 
     @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception{
+    public AuthenticationManager authenticationManager(HttpSecurity http)
+            throws Exception {
         return http.getSharedObject(AuthenticationManagerBuilder.class)
-                 .userDetailsService(userDetailServices)
-                 .passwordEncoder(passwordEncoder())
-                 .and()
-                 .build();
+                .userDetailsService(userDetailService)
+                .passwordEncoder(passwordEncoder())
+                .and()
+                .build();
     }
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception{
         http
-
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/info").permitAll()
+                .antMatchers(HttpMethod.POST, "/sign-up").permitAll()
                 .anyRequest().authenticated().and()
                 .httpBasic();
 
         return http.build();
     }
-
-//    @Bean
-//    public UserDetailsService userDetailsService (){
-//
-//        UserDetails admin = User.builder()
-//                .username("admin")
-//                .password(passwordEncoder().encode("123"))
-//                .roles("ADMIN")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(admin);
-//    }
 }
